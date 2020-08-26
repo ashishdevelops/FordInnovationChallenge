24fb641e109a53b1368b5d7b312601023a0cedba
Ashish Patel <apatel.developer@gmail.com>
2020-08-25 21:47:25 -0400

    final commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# InternInnovationDemo
+printf("ord") team demo of coronavirus safety alerts
+
+Created by Ashish Patel (APATE207) and Jonathan Parks (JPARKS41)
+
+<b>A Quick demo of our Health Alerts POC. Check out the Powerpoint presentation in the Repo for our pitch!</b>
+
+<b>Open the test.html file in a browser to interact! local JavaScript is Required!</b>
+
+Left side is to simulated movement into new counties.
+Right side is simulate notification on a Mach-E dashboard
+
+Sorry for the rushed look and some absolute element positioning, we didn't have much development time! :)
+
+
---
+++ app.js
@@ -0,0 +1,721 @@
+data = {
+"Alcona": {
+"CASE_STATUS": "Confirmed",
+"Cases": 20,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Alger": {
+"CASE_STATUS": "Confirmed",
+"Cases": 3,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Allegan": {
+"CASE_STATUS": "Confirmed",
+"Cases": 345,
+"Deaths": 7,
+"Updated": "2020/07/14 13:05:53"
+},
+"Alpena": {
+"CASE_STATUS": "Confirmed",
+"Cases": 111,
+"Deaths": 12,
+"Updated": "2020/07/14 13:05:53"
+},
+"Antrim": {
+"CASE_STATUS": "Confirmed",
+"Cases": 25,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Arenac": {
+"CASE_STATUS": "Confirmed",
+"Cases": 34,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Baraga": {
+"CASE_STATUS": "Confirmed",
+"Cases": 6,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Barry": {
+"CASE_STATUS": "Confirmed",
+"Cases": 116,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Bay": {
+"CASE_STATUS": "Confirmed",
+"Cases": 396,
+"Deaths": 31,
+"Updated": "2020/07/14 13:05:53"
+},
+"Benzie": {
+"CASE_STATUS": "Confirmed",
+"Cases": 14,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Berrien": {
+"CASE_STATUS": "Confirmed",
+"Cases": 859,
+"Deaths": 61,
+"Updated": "2020/07/14 13:05:53"
+},
+"Branch": {
+"CASE_STATUS": "Confirmed",
+"Cases": 254,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Calhoun": {
+"CASE_STATUS": "Confirmed",
+"Cases": 552,
+"Deaths": 38,
+"Updated": "2020/07/14 13:05:53"
+},
+"Cass": {
+"CASE_STATUS": "Confirmed",
+"Cases": 180,
+"Deaths": 6,
+"Updated": "2020/07/14 13:05:53"
+},
+"Charlevoix": {
+"CASE_STATUS": "Confirmed",
+"Cases": 28,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Cheboygan": {
+"CASE_STATUS": "Confirmed",
+"Cases": 24,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Chippewa": {
+"CASE_STATUS": "Confirmed",
+"Cases": 20,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Clare": {
+"CASE_STATUS": "Confirmed",
+"Cases": 33,
+"Deaths": 3,
+"Updated": "2020/07/14 13:05:53"
+},
+"Clinton": {
+"CASE_STATUS": "Confirmed",
+"Cases": 245,
+"Deaths": 12,
+"Updated": "2020/07/14 13:05:53"
+},
+"Crawford": {
+"CASE_STATUS": "Confirmed",
+"Cases": 79,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Delta": {
+"CASE_STATUS": "Confirmed",
+"Cases": 28,
+"Deaths": 3,
+"Updated": "2020/07/14 13:05:53"
+},
+"Detroit City": {
+"CASE_STATUS": "Confirmed",
+"Cases": 11995,
+"Deaths": 1459,
+"Updated": "2020/07/14 13:05:53"
+},
+"Dickinson": {
+"CASE_STATUS": "Confirmed",
+"Cases": 20,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Eaton": {
+"CASE_STATUS": "Confirmed",
+"Cases": 287,
+"Deaths": 7,
+"Updated": "2020/07/14 13:05:53"
+},
+"Emmet": {
+"CASE_STATUS": "Confirmed",
+"Cases": 35,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"FCI": {
+"CASE_STATUS": "Confirmed",
+"Cases": 183,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Genesee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 2338,
+"Deaths": 266,
+"Updated": "2020/07/14 13:05:53"
+},
+"Gladwin": {
+"CASE_STATUS": "Confirmed",
+"Cases": 28,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Gogebic": {
+"CASE_STATUS": "Confirmed",
+"Cases": 15,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Grand Traverse": {
+"CASE_STATUS": "Confirmed",
+"Cases": 97,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Gratiot": {
+"CASE_STATUS": "Confirmed",
+"Cases": 97,
+"Deaths": 14,
+"Updated": "2020/07/14 13:05:53"
+},
+"Hillsdale": {
+"CASE_STATUS": "Confirmed",
+"Cases": 196,
+"Deaths": 25,
+"Updated": "2020/07/14 13:05:53"
+},
+"Houghton": {
+"CASE_STATUS": "Confirmed",
+"Cases": 25,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Huron": {
+"CASE_STATUS": "Confirmed",
+"Cases": 70,
+"Deaths": 3,
+"Updated": "2020/07/14 13:05:53"
+},
+"Ingham": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1165,
+"Deaths": 29,
+"Updated": "2020/07/14 13:05:53"
+},
+"Ionia": {
+"CASE_STATUS": "Confirmed",
+"Cases": 188,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Iosco": {
+"CASE_STATUS": "Confirmed",
+"Cases": 105,
+"Deaths": 10,
+"Updated": "2020/07/14 13:05:53"
+},
+"Iron": {
+"CASE_STATUS": "Confirmed",
+"Cases": 8,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Isabella": {
+"CASE_STATUS": "Confirmed",
+"Cases": 139,
+"Deaths": 8,
+"Updated": "2020/07/14 13:05:53"
+},
+"Jackson": {
+"CASE_STATUS": "Confirmed",
+"Cases": 534,
+"Deaths": 31,
+"Updated": "2020/07/14 13:05:53"
+},
+"Kalamazoo": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1168,
+"Deaths": 70,
+"Updated": "2020/07/14 13:05:53"
+},
+"Kalkaska": {
+"CASE_STATUS": "Confirmed",
+"Cases": 29,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Kent": {
+"CASE_STATUS": "Confirmed",
+"Cases": 5483,
+"Deaths": 142,
+"Updated": "2020/07/14 13:05:53"
+},
+"Keweenaw": {
+"CASE_STATUS": "Confirmed",
+"Cases": 2,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Lake": {
+"CASE_STATUS": "Confirmed",
+"Cases": 13,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Lapeer": {
+"CASE_STATUS": "Confirmed",
+"Cases": 275,
+"Deaths": 31,
+"Updated": "2020/07/14 13:05:53"
+},
+"Leelanau": {
+"CASE_STATUS": "Confirmed",
+"Cases": 44,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Lenawee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 248,
+"Deaths": 11,
+"Updated": "2020/07/14 13:05:53"
+},
+"Livingston": {
+"CASE_STATUS": "Confirmed",
+"Cases": 495,
+"Deaths": 27,
+"Updated": "2020/07/14 13:05:53"
+},
+"Luce": {
+"CASE_STATUS": "Confirmed",
+"Cases": 3,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"MDOC": {
+"CASE_STATUS": "Confirmed",
+"Cases": 4113,
+"Deaths": 68,
+"Updated": "2020/07/14 13:05:53"
+},
+"Mackinac": {
+"CASE_STATUS": "Confirmed",
+"Cases": 9,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Macomb": {
+"CASE_STATUS": "Confirmed",
+"Cases": 7670,
+"Deaths": 892,
+"Updated": "2020/07/14 13:05:53"
+},
+"Manistee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 26,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Marquette": {
+"CASE_STATUS": "Confirmed",
+"Cases": 89,
+"Deaths": 11,
+"Updated": "2020/07/14 13:05:53"
+},
+"Mason": {
+"CASE_STATUS": "Confirmed",
+"Cases": 71,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Mecosta": {
+"CASE_STATUS": "Confirmed",
+"Cases": 36,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Menominee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 46,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Midland": {
+"CASE_STATUS": "Confirmed",
+"Cases": 145,
+"Deaths": 9,
+"Updated": "2020/07/14 13:05:53"
+},
+"Missaukee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 25,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Monroe": {
+"CASE_STATUS": "Confirmed",
+"Cases": 581,
+"Deaths": 22,
+"Updated": "2020/07/14 13:05:53"
+},
+"Montcalm": {
+"CASE_STATUS": "Confirmed",
+"Cases": 120,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Montmorency": {
+"CASE_STATUS": "Confirmed",
+"Cases": 7,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Muskegon": {
+"CASE_STATUS": "Confirmed",
+"Cases": 924,
+"Deaths": 54,
+"Updated": "2020/07/14 13:05:53"
+},
+"Newaygo": {
+"CASE_STATUS": "Confirmed",
+"Cases": 193,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Oakland": {
+"CASE_STATUS": "Confirmed",
+"Cases": 9645,
+"Deaths": 1068,
+"Updated": "2020/07/14 13:05:53"
+},
+"Oceana": {
+"CASE_STATUS": "Confirmed",
+"Cases": 391,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Ogemaw": {
+"CASE_STATUS": "Confirmed",
+"Cases": 33,
+"Deaths": 2,
+"Updated": "2020/07/14 13:05:53"
+},
+"Ontonagon": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Osceola": {
+"CASE_STATUS": "Confirmed",
+"Cases": 47,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Oscoda": {
+"CASE_STATUS": "Confirmed",
+"Cases": 17,
+"Deaths": 1,
+"Updated": "2020/07/14 13:05:53"
+},
+"Otsego": {
+"CASE_STATUS": "Confirmed",
+"Cases": 107,
+"Deaths": 10,
+"Updated": "2020/07/14 13:05:53"
+},
+"Ottawa": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1250,
+"Deaths": 48,
+"Updated": "2020/07/14 13:05:53"
+},
+"Out-of-State": {
+"CASE_STATUS": "Confirmed",
+"Cases": 52,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Presque Isle": {
+"CASE_STATUS": "Confirmed",
+"Cases": 14,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Roscommon": {
+"CASE_STATUS": "Confirmed",
+"Cases": 30,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Saginaw": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1361,
+"Deaths": 122,
+"Updated": "2020/07/14 13:05:53"
+},
+"Sanilac": {
+"CASE_STATUS": "Confirmed",
+"Cases": 53,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Schoolcraft": {
+"CASE_STATUS": "Confirmed",
+"Cases": 7,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Shiawassee": {
+"CASE_STATUS": "Confirmed",
+"Cases": 260,
+"Deaths": 27,
+"Updated": "2020/07/14 13:05:53"
+},
+"St Clair": {
+"CASE_STATUS": "Confirmed",
+"Cases": 525,
+"Deaths": 52,
+"Updated": "2020/07/14 13:05:53"
+},
+"St Joseph": {
+"CASE_STATUS": "Confirmed",
+"Cases": 352,
+"Deaths": 5,
+"Updated": "2020/07/14 13:05:53"
+},
+"Tuscola": {
+"CASE_STATUS": "Confirmed",
+"Cases": 234,
+"Deaths": 27,
+"Updated": "2020/07/14 13:05:53"
+},
+"Unknown": {
+"CASE_STATUS": "Confirmed",
+"Cases": 23,
+"Deaths": 0,
+"Updated": "2020/07/14 13:05:53"
+},
+"Van Buren": {
+"CASE_STATUS": "Confirmed",
+"Cases": 248,
+"Deaths": 8,
+"Updated": "2020/07/14 13:05:53"
+},
+"Washtenaw": {
+"CASE_STATUS": "Confirmed",
+"Cases": 1715,
+"Deaths": 108,
+"Updated": "2020/07/14 13:05:53"
+},
+"Wayne": {
+"CASE_STATUS": "Confirmed",
+"Cases": 11186,
+"Deaths": 1183,
+"Updated": "2020/07/14 13:05:53"
+},
+"Wexford": {
+"CASE_STATUS": "Confirmed",
+"Cases": 43,
+"Deaths": 4,
+"Updated": "2020/07/14 13:05:53"
+},
+"": {
+"CASE_STATUS": "",
+"Cases": "",
+"Deaths": "",
+"Updated": ""
+}
+};
+
+crimeData = {
+    "Allegan": "MI0310300",
+    "Alpena": "MI0410400",
+    "Arenac": "MI0610600",
+    "Barry": "MI0810800",
+    "Bay": "MI0910900",
+    "Berrien": "MI1111100",
+    "Branch": "MI1211200",
+    "Calhoun": "MI1311300",
+    "Cass": "MI1411400",
+    "Chippewa": "MI1711700",
+    "Clare": "MI1811800",
+    "Clinton": "MI1911900",
+    "Genesee": "MI2512500",
+    "Gogebic": "MI2712700",
+    "Gratiot": "MI2912900",
+    "Huron": "MI3213200",
+    "Iron": "MI3613600",
+    "Jackson": "MI3813800",
+    "Kalkaska": "MI4014000",
+    "Kent": "MI4114100",
+    "Lenawee": "MI4614600",
+    "Mackinac": "MI4914900",
+    "Macomb": "MI5015000",
+    "Manistee": "MI5115100",
+    "Marquette": "MI5215200",
+    "Montmorency": "MI6016000",
+    "Muskegon": "MI6116100",
+    "Oscoda": "MI6816800",
+    "Otsego": "MI6916900",
+    "Ottawa": "MI7017000",
+    "Roscommon": "MI7217200",
+    "Sanilac": "MI7617600",
+    "Schoolcraft": "MI7717700",
+    "Shiawassee": "MI7817800",
+    "St Joseph": "MI7517500",
+    "Van Buren": "MI8018000",
+    "Washtenaw": "MI8118100",
+    "Wexford": "MI8318300"
+};
+
+(function(exports) {
+    "use strict";
+
+    function initMap() {
+    exports.map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+        lat: 43.93,
+        lng: -85.73
+        },
+        zoom: 6
+    });
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+
+    }
+    
+    function placeMarker(location) {
+
+        var marker = new google.maps.Marker({
+            position: location, 
+            map: map
+        });
+        
+        //make FCC api call, and change element info
+        var part_URL = 'https://geo.fcc.gov/api/census/area?';
+        var lat = 'lat='.concat(marker.getPosition().lat(),'&');
+        var long = 'lon='.concat(marker.getPosition().lng());
+        var full_URL = part_URL.concat(lat,long);
+
+        //get county from lat long
+        fetch(full_URL)
+        .then((response) => {
+            return response.json(); // or .text() or .blob() ...
+        })
+        .then((text) => {
+
+            var county_name = text["results"]["0"]["county_name"];
+            //covid notif
+            notif(county_name, "covid", marker);
+            notif(county_name, "crime", marker);
+
+            console.log("done");
+
+
+        })
+        .catch((e) => {
+            marker.setMap(null);
+        });
+    }
+    exports.initMap = initMap;
+})((this.window = this.window || {}));
+
+
+function notif(county_name, type, marker){
+    if(type == "covid"){
+        var case_num = data[county_name]["Cases"];
+        var notification_insert = `${"Awareness".bold()}: ${county_name} has ${case_num} COVID-19 cases. Please follow CDC guidelines and exercise caution.`;
+        document.getElementById("notification_text").innerHTML = notification_insert;
+
+        unfade(document.getElementById("notification_box"));
+        setTimeout(function() {
+            console.log("work");
+            fade(document.getElementById("notification_box"));
+            marker.setMap(null);
+        }, 3500);
+    } else if(type == "crime"){
+        var opi = crimeData[county_name];
+        var stolenCars = 0;
+        //add crime api call
+        full_URL = "https://api.usa.gov/crime/fbi/sapi/api/summarized/agencies/";
+        full_URL = full_URL.concat(opi);
+        full_URL = full_URL.concat("/offenses/2018/2018?API_KEY=<APIKEY>");
+        fetch(full_URL)
+        .then((response) => {
+            return response.json(); // or .text() or .blob() ...
+        })
+        .then((text) => {
+          var i;
+          console.log(text);
+          for (i = 0; i < text["results"].length; i++) {
+            if(text["results"][i]["offense"] == "motor-vehicle-theft")
+            {
+              stolenCars = text["results"][i]["actual"];
+              var notification_insert = `${"Awareness".bold()}: ${county_name} had ${stolenCars} Motor Vehicle Thefts reported this year. Please take necessary precautions.`;
+              document.getElementById("notification_text2").innerHTML = notification_insert;
+      
+              unfade(document.getElementById("notification_box2"));
+              setTimeout(function() {
+                  fade(document.getElementById("notification_box2"));
+                  marker.setMap(null);
+              }, 3500);
+              break;
+            }
+          }
+        })
+        .catch((e) => {
+            console.log("L in crime");
+        });
+    }
+    
+
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function hideElem() {
+    document.getElementById("notification_box").style.visibility = "hidden"; 
+}
+
+function showElem() {
+    document.getElementById("notification_box").style.visibility = "visible";
+}
+
+window.onload = function() {
+    fade(document.getElementById("notification_box"));
+    fade(document.getElementById("notification_box2"));
+}
